0d22546c1bcbb174e8d5dde3d38b0b3f05d38cd4
alantr7 <alan.trumic066@gmail.com>
2026-03-01 19:58:05 +0100

    feat: api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BACKEND_URL,
+        changeOrigin: true,
+      }
+    }
+  }
 })
