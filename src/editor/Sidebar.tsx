7672c522314b7097807643fcc17348daebc906dd
alantr7 <alan.trumic066@gmail.com>
2026-03-01 14:35:58 +0100

    feat: sidebar button styles and icons

--- src/editor/Sidebar.tsx
+++ src/editor/Sidebar.tsx
@@ -2,10 +2,14 @@ export default function Sidebar() {
     return <div className="sidebar">
         <div className="files-container">
             <div className="file active">file-1</div>
+            <hr />
+            <a href="https://github.com/alantr7/codebots/wiki/Scripting-Language" target="_blank">
+                <div className="file docs">SCRIPT DOCS</div>
+            </a>
         </div>
         <div className="footer">
-            <p>CodeBots | Editor v2.0</p>
-            <button>What's new?</button>
+            <p>cbs-editor v2.0</p>
+            <button>see changelog</button>
         </div>
     </div>
 }
